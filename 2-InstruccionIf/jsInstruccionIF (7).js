d6435e5f1c4e26fd382666b6f17191943955f4bc
estebansanchezfg <60760303+estebansanchezfg@users.noreply.github.com>
2020-02-13 21:58:52 -0300

    cambios del 13/03
    asdasdasd

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estado;
+edad = parseint(document.getElementById("edad").value);
+estado = document.getElementById("estadocivil").value;
 
-	
+if( edad < 18 && estado != "soltero"){
 
+    alert("es muy pequeño para no ser soltero")
+}
 
 }//FIN DE LA FUNCIÓN
