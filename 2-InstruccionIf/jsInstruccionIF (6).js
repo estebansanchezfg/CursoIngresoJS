67a28a27c6f1d936ac75d150de1f90f81aa35b23
estebansanchezfg <60760303+estebansanchezfg@users.noreply.github.com>
2020-02-13 21:37:14 -0300

    Add files via upload
    patata

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
 
-
-
-
+if("edad" < 13){
+    alert("niño");
+}
+else if (edad <= 17){
+    alert("adolescente");
+}
+else {
+    alert("adulto");
+}
 }//FIN DE LA FUNCIÓN
