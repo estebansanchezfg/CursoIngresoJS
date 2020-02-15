eddac5581b4f7045d0fa2b5c62aebec96b81b62e
estebansanchezfg <60760303+estebansanchezfg@users.noreply.github.com>
2020-02-14 21:53:59 -0300

    Add files via upload
    cosas del 14/2

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,19 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mes)  {
+
+    case "enero":
+        alert("que comiences bien el año");
+        break;
+    case "marzo":
+        alert("a clases!!!");
+        break;
+    case "julio":
+        alert("se vienen las vacaciones!!");
+        break;     
+
+}
 
 
 
