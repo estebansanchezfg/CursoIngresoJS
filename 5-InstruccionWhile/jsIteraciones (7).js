258525b0bcee8c5dd790748a4f48ae2abd1214b4
estebansanchezfg <60760303+estebansanchezfg@users.noreply.github.com>
2020-02-21 20:30:17 -0300

    21/2 °2
    parte 2

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,28 @@
 function mostrar()
 {
-
+    var numero;
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var seguir = "5";
 
+	do {
+		
+		numero = parseint(promp("ingrese un numero: "))
+		while (isNaN(numero)){
+			numero = parseInt(prompt(" eso no es seguro.ingrese un numero:"));
+		}
+		   acumulador = acumulador + numero;
+		   
+		   cortadores;
+
+
+
+		seguir = prompt("quiere ingresar otro numero?");
+	} while (seguir == "s")
+promedio =acumulador / contador;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,37 @@
 function mostrar()
 {
 
-	var contador=0;
+	var flag=0;
 	var positivo=0;
 	var negativo=1;
 	
 	var respuesta='si';
+	do {
+		
+		numero = parseint(promp("ingrese un numero: "))
+		while (isNaN(numero)){
+			numero = parseInt(prompt(" eso no es seguro.ingrese un numero:"));
+		}
+		   
+		if( numero >= 0)     {
+			positivo = positivo + numero;
+
+
+		}else{
+			negativo = negativo * numero
+            flag = 1;
+
+		}
+
+
+		seguir = prompt("quiere ingresar otro numero?");
+	} while (seguir == "s");
+
+	if( flag == 0){
+		negativo= 0;
+	}
+
+
 
 
 document.getElementById('suma').value=positivo;
