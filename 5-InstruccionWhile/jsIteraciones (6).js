15a8ec156f8ed82b53177f76c71f697e9c88ab99
estebansanchezfg <60760303+estebansanchezfg@users.noreply.github.com>
2020-02-20 22:01:46 -0300

    uff
    dios

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,7 +3,7 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var numero;
+	var promedio;
 while(contador <5){
 
 numero = parseint(prompt("inserte un numero: "));
@@ -12,6 +12,6 @@ contador++
 }
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=promedio/5;
 
 }//FIN DE LA FUNCIÓN
